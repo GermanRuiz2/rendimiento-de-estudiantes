eecffdb574576c5e2976b52d06b99f6c85013f16
GermanRuiz2 <german.erz2000@gmail.com>
2026-09-17 15:53:55 -0600

    Código para la exploración inicial de los datos y reparación de error al abrir el dataset por no ser encontrado

--- src/analisis.py
+++ src/analisis.py
@@ -3,8 +3,44 @@ import pandas as pd
 from pathlib import Path
 
 ruta_raiz = Path(__file__).resolve().parent.parent
-ruta_archivo = ruta_raiz / "data" / "archivo.csv"
+ruta_archivo = ruta_raiz / "data" / "StudentsPerformance.csv"
 
 df = pd.read_csv(ruta_archivo)
 
 print("Data Set Cargado Con Éxito")
+print("\n==============\n EXPLORACION INICIAL: \n\n")
+
+# Primeras Filas
+print("Primeras filas:")
+print(df.head())
+
+# Registros y columnas
+filas, columnas = df.shape
+print(f"\nDimensiones: {filas} filas y {columnas} columnas")
+
+# Nombres Variables
+print("\nVariables en el dataset:")
+print(df.columns.tolist())
+
+# Tipos 
+print("\nTipos de datos:")
+print(df.dtypes)
+
+# Resumen general (combina no nulos, tipos y memoria)
+print("\nResumen general:")
+df.info()
+
+# Datos nulos:
+print("\nValores faltantes por variable:")
+print(df.isnull().sum())
+
+# Registros duplicados totales
+duplicados = df.duplicated().sum()
+print(f"\nRegistros duplicados: {duplicados}")
+
+print("\nEstadísticas variables numéricas:")
+print(df.describe())
+
+# Estadísticas variables categóricas / texto
+print("\nEstadísticas variables categóricas:")
+print(df.describe(include=["object", "category"]))
